fe14bc56b8c1a4e8f06220dbcd090a3f166acd6e
mutjin08 <mutjin08@gmail.com>
2024-03-25 10:09:05 +0900

    enviornment setting

--- hello/src/test/java/com/hana/HelloApplicationTests.java
+++ hello/src/test/java/com/hana/HelloApplicationTests.java
@@ -2,13 +2,14 @@ package com.hana;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//import lombok.extern.slf4j.*;
+
 
 @SpringBootTest
 class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//System.out.println("test");
 	}
 
 }
